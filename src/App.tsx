5bcf3867088cac230621f3c5ddb2f72cd5d3d06d
Franco-04 <davidfranco7329@gmail.com>
2025-06-09 20:12:39 -0600

    Agregando pexel-api-key

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const [error, setError] = useState<string | null>(null);
   const [inputSearch, setInputSearch] = useState<string>('');
 
-  const PEXELS_API_KEY: string = 'TU_CLAVE_PEXELS';
+  const PEXELS_API_KEY: string = 'Fi2EeO0i2hnYXrh6vaP9lB0Z6AbajkRpvX48TQ429hP8u94RlunJhf4G';
 
   useEffect(() => {
     fetchPhotos(query);
